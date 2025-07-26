1441481e2b8d7f34a9dd6659df1056ecbec886d3
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-07-26 14:46:35 +0300

    Refactor

--- apps/server/src/middleware/error-handler.ts
+++ apps/server/src/middleware/error-handler.ts
@@ -1,16 +1,23 @@
-import { type NextFunction, type Request, type Response } from "express";
+import type { NextFunction, Request, Response } from "express";
+import { STATUS_CODES } from "http";
 import createHttpError from "http-errors";
 
 export default [
     (_req: Request, _res: Response, next: NextFunction) =>
         next(createHttpError(404)),
     (err: Error, _req: Request, res: Response, _next: NextFunction) => {
-        const code = createHttpError.isHttpError(err) ? err.status : 500;
+        const httpError = createHttpError.isHttpError(err);
+
+        const code = httpError ? err.status : 500;
 
         if (code >= 500) {
             console.error(err);
         }
 
-        res.sendStatus(code);
+        res.status(code).json({
+            status: code,
+            message: httpError && err.expose ? err.message : STATUS_CODES[code],
+            data: httpError && err.expose ? err.data : undefined,
+        });
     },
 ];
