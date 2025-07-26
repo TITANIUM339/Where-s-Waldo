f12e70c4838dd00276a853427fd2994e06ea788f
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-07-26 14:36:33 +0300

    Create index route

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import ErrorBoundary from "./layouts/root/ErrorBoundary";
 import Root from "./layouts/root/Root";
+import indexAction from "./pages/index/action";
+import Index from "./pages/index/Index";
+import indexLoader from "./pages/index/loader";
 
 export default function App() {
     const router = createBrowserRouter([
@@ -8,7 +11,14 @@ export default function App() {
             path: "/",
             Component: Root,
             ErrorBoundary,
-            children: [],
+            children: [
+                {
+                    index: true,
+                    loader: indexLoader,
+                    action: indexAction,
+                    Component: Index,
+                },
+            ],
         },
     ]);
 
