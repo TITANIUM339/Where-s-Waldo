ce1f80f2b987f304d109eb6220c5261087a41d7a
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-07-26 14:36:04 +0300

    Create index page

---
+++ apps/client/src/pages/index/Index.tsx
@@ -0,0 +1,56 @@
+import { useId } from "react";
+import { Form, useActionData, useNavigation } from "react-router";
+import Button from "../../components/Button";
+import Spinner from "../../components/Spinner";
+import type { APIerror } from "../../types/api-types";
+
+export default function Index() {
+    const id = useId();
+
+    const navigation = useNavigation();
+
+    const error: APIerror | undefined = useActionData();
+
+    const nameErrorMessage = error?.data?.find(
+        ({ path }) => path === "name",
+    )?.msg;
+
+    return (
+        <div className="flex h-full items-center justify-center">
+            <div className="rounded-md bg-gray-50 p-12 shadow dark:bg-gray-950">
+                <Form method="post" className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-2">
+                        <label className="text-lg font-medium" htmlFor={id}>
+                            Name *
+                        </label>
+                        <input
+                            className="rounded-md border border-gray-200 bg-gray-100 p-2 dark:border-gray-800 dark:bg-gray-900"
+                            id={id}
+                            name="name"
+                            autoFocus
+                            autoComplete="name"
+                            type="text"
+                            maxLength={16}
+                            required
+                        />
+                        {nameErrorMessage ? (
+                            <p className="text-red-600">{nameErrorMessage}</p>
+                        ) : null}
+                    </div>
+                    <Button
+                        type="submit"
+                        disabled={navigation.state !== "idle"}
+                    >
+                        {navigation.state === "idle" ? (
+                            "Continue"
+                        ) : (
+                            <div className="flex justify-center">
+                                <Spinner size={20} />
+                            </div>
+                        )}
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    );
+}
