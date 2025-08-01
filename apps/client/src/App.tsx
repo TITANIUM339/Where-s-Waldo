ede7c991d8d951f6fe531d4f772c3a3e710a40d4
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-01 20:36:46 +0300

    Create games route

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import ErrorBoundary from "./layouts/root/ErrorBoundary";
 import Root from "./layouts/root/Root";
+import Games from "./pages/games/Games";
+import gamesLoader from "./pages/games/loader";
 import indexAction from "./pages/index/action";
 import Index from "./pages/index/Index";
 import indexLoader from "./pages/index/loader";
@@ -17,6 +19,11 @@ export default function App() {
                     loader: indexLoader,
                     action: indexAction,
                     Component: Index,
+                },
+                {
+                    path: "games",
+                    loader: gamesLoader,
+                    Component: Games,
                 },
             ],
         },
