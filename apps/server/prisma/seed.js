2b5680ec9598707286e4bc4c3a84b7b6ce429289
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-01 20:26:20 +0300

    Create db seed script

---
+++ apps/server/prisma/seed.js
@@ -0,0 +1,39 @@
+import { PrismaClient } from "../generated/index.js";
+
+const prisma = new PrismaClient();
+
+const games = await prisma.game.createManyAndReturn({
+    data: [
+        {
+            name: "Horseplay in Troy",
+            description:
+                "What a spectacular scene this is, Waldo-Watchers? And what an epic commotion picture! I wonder why the trojans didn't guess the wooden horse was full of greeks, and how did they get it through the gates of Troy anyway? I wouldn't like to be in the trojans sandals, if the costume department had givin them any, that is!",
+            imageURL: "https://i.imgur.com/QLACwOG.jpeg",
+            imageWidth: 3536,
+            imageHeight: 2227,
+        },
+    ],
+});
+
+const characters = await prisma.character.createManyAndReturn({
+    data: [{ name: "Waldo" }],
+});
+
+function getGameId(name) {
+    return games.find(({ name: n }) => n === name).id;
+}
+
+function getCharacterId(name) {
+    return characters.find(({ name: n }) => n === name).id;
+}
+
+await prisma.coordinate.createMany({
+    data: [
+        {
+            x: 554,
+            y: 1858,
+            characterId: getCharacterId("Waldo"),
+            gameId: getGameId("Horseplay in Troy"),
+        },
+    ],
+});
