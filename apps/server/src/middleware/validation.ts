6c647b93cebca8c8dc36d880ee853a38d0a112e4
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-06 18:14:26 +0300

    Validate new-game route param

--- apps/server/src/middleware/validation.ts
+++ apps/server/src/middleware/validation.ts
@@ -1,10 +1,22 @@
-import { body } from "express-validator";
+import { body, param } from "express-validator";
+import prisma from "../lib/prisma.js";
 
-export function validateNewPlayer() {
+export function validateNewPlayerBody() {
     return body("name")
         .trim()
         .notEmpty()
         .withMessage("required")
         .isLength({ max: 16 })
         .withMessage("maximum length is 16");
+}
+
+export function validateNewGameParam() {
+    return param("gameId")
+        .isInt({ min: 1 })
+        .bail()
+        .toInt()
+        .custom(
+            async (value) =>
+                await prisma.game.findUniqueOrThrow({ where: { id: value } }),
+        );
 }
