e4ee5feae700889f73389cd9cce7f5c92de235a4
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-09 20:43:52 +0300

    Create timer

---
+++ apps/client/src/components/Timer.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export default function Timer({ start }: { start: number }) {
+    const [timer, setTimer] = useState({ start, now: start });
+
+    useEffect(() => {
+        const interval = setInterval(
+            () => setTimer((prev) => ({ ...prev, now: Date.now() })),
+            10,
+        );
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const elapsedSeconds = (timer.now - timer.start) / 1000;
+
+    const hours = Math.floor(elapsedSeconds / 3600);
+    const minutes = (Math.floor(elapsedSeconds / 60) % 60).toLocaleString(
+        "en-US",
+        {
+            minimumIntegerDigits: 2,
+        },
+    );
+    const seconds = (elapsedSeconds % 60).toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+    });
+
+    return (
+        <time className="bg-gray-50/60 p-2 shadow dark:bg-gray-950/60">
+            {hours}:{minutes}:{seconds}
+        </time>
+    );
+}
