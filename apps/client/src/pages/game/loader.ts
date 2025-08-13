086baedd765f22db80822ec3efaf14d86f5e4b9b
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-13 22:18:47 +0300

    Redirect to index page if there is no player

--- apps/client/src/pages/game/loader.ts
+++ apps/client/src/pages/game/loader.ts
@@ -1,9 +1,16 @@
 /* eslint-disable @typescript-eslint/only-throw-error */
 import type { LoaderFunctionArgs } from "react-router";
+import { redirect } from "react-router";
 import api from "../../lib/api";
 import type { APInewGame } from "../../types/api-types";
 
 export default async function loader({ params }: LoaderFunctionArgs) {
+    const player = localStorage.getItem("player-token");
+
+    if (!player) {
+        return redirect("/");
+    }
+
     const response = await api(`games/${params.gameId}/new-game`);
 
     if (!response.ok) {
--- apps/client/src/pages/games/loader.ts
+++ apps/client/src/pages/games/loader.ts
@@ -1,8 +1,15 @@
 /* eslint-disable @typescript-eslint/only-throw-error */
+import { redirect } from "react-router";
 import api from "../../lib/api";
 import type { APIgames } from "../../types/api-types";
 
 export default async function loader() {
+    const player = localStorage.getItem("player-token");
+
+    if (!player) {
+        return redirect("/");
+    }
+
     const response = await api("games");
 
     if (!response.ok) {
