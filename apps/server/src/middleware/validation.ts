0b9f209a06fa7e608c925b58fffade408de89108
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-17 12:10:09 +0300

    Validate end-game body

--- apps/server/src/middleware/validation.ts
+++ apps/server/src/middleware/validation.ts
@@ -1,5 +1,7 @@
-import { param, query } from "express-validator";
+import { body, matchedData, param, query } from "express-validator";
+import isInProximity from "../lib/isInProximity.js";
 import prisma from "../lib/prisma.js";
+import verifyJwt from "../lib/verifyJwt.js";
 
 export function validateNewPlayerQuery() {
     return query("name")
@@ -44,5 +46,87 @@ export function validateCharacterPositionQuery() {
             .isNumeric()
             .withMessage("must be number")
             .custom((value) => Number(value)),
+    ];
+}
+
+export function validateEndGameBody() {
+    return [
+        body("characters.*.id")
+            .isInt({ min: 1 })
+            .withMessage("must be integer")
+            .bail()
+            .toInt()
+            .custom(
+                async (value, { req }) =>
+                    await prisma.coordinate.findUniqueOrThrow({
+                        where: {
+                            id: {
+                                characterId: value,
+                                gameId: matchedData(req).gameId,
+                            },
+                        },
+                    }),
+            )
+            .withMessage("character does not exist"),
+        body("characters.*.position.x")
+            .isNumeric()
+            .withMessage("must be number")
+            .customSanitizer((value) => Number(value)),
+        body("characters.*.position.y")
+            .isNumeric()
+            .withMessage("must be number")
+            .customSanitizer((value) => Number(value)),
+        body("characters")
+            .isArray()
+            .withMessage("must be array")
+            .bail()
+            .custom(async (value, { req }) => {
+                const coordinates = await prisma.coordinate.findMany({
+                    where: { gameId: matchedData(req).gameId },
+                    orderBy: { characterId: "asc" },
+                });
+
+                value.sort(
+                    ({ id: a }: { id: number }, { id: b }: { id: number }) =>
+                        a - b,
+                );
+
+                for (let i = 0; i < coordinates.length; i++) {
+                    const a = { x: coordinates[i]?.x, y: coordinates[i]?.y };
+                    const b = {
+                        x: value[i]?.position?.x,
+                        y: value[i]?.position?.y,
+                    };
+
+                    if (!isInProximity(a, b)) {
+                        throw new Error("invalid value");
+                    }
+                }
+            }),
+        body("playerToken")
+            .notEmpty()
+            .withMessage("required")
+            .custom(async (value) => {
+                const data = await verifyJwt(value);
+
+                if (
+                    typeof (data as { id?: unknown }).id !== "string" ||
+                    typeof (data as { name?: unknown }).name !== "string"
+                ) {
+                    throw new Error();
+                }
+            })
+            .withMessage("invalid token"),
+        body("gameToken")
+            .notEmpty()
+            .withMessage("required")
+            .custom(async (value) => {
+                const data = await verifyJwt(value);
+
+                if (typeof (data as { iat?: unknown }).iat !== "number") {
+                    throw new Error();
+                }
+            })
+            .withMessage("invalid token"),
     ];
 }
