7695a12ab5f4ea0c39674167b20dee38915437a0
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-17 12:13:32 +0300

    Add end-game router

--- apps/server/src/routes/games/game/game.ts
+++ apps/server/src/routes/games/game/game.ts
@@ -2,12 +2,20 @@ import { Router } from "express";
 import { paramHandler } from "../../../middleware/validation-error-handler.js";
 import { validateGameIdParam } from "../../../middleware/validation.js";
 import characters from "./characters/characters.js";
+import endGame from "./end-game/end-game.js";
 import newGame from "./new-game/new-game.js";
 
 const router = Router();
 
 const route = "/:gameId";
 
-router.use(route, validateGameIdParam(), paramHandler, newGame, characters);
+router.use(
+    route,
+    validateGameIdParam(),
+    paramHandler,
+    newGame,
+    characters,
+    endGame,
+);
 
 export default router;
