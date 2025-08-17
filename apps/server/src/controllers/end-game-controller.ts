d4f39e9e4893373cd71c50e1a24be92914c4c1bf
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-17 12:08:15 +0300

    Create new Leaderboard entry

---
+++ apps/server/src/controllers/end-game-controller.ts
@@ -0,0 +1,36 @@
+import type { Request, Response } from "express";
+import { matchedData } from "express-validator";
+import prisma from "../lib/prisma.js";
+import verifyJwt from "../lib/verifyJwt.js";
+
+interface Player {
+    id: string;
+    name: string;
+}
+
+interface Game {
+    iat: number;
+}
+
+export default {
+    async post(req: Request, res: Response) {
+        const { playerToken, gameToken } = matchedData(req);
+
+        const [player, game] = await Promise.all([
+            verifyJwt(playerToken),
+            verifyJwt(gameToken),
+        ]);
+
+        const id = (player as Player).id;
+        const name = (player as Player).name;
+        const start = new Date((game as Game).iat * 1000);
+
+        const entry = await prisma.leaderboard.upsert({
+            create: { id, name, start },
+            update: { start },
+            where: { id },
+        });
+
+        res.status(201).json(entry);
+    },
+};
--- apps/server/src/lib/constants.ts
+++
@@ -1,1 +0,0 @@
-export const TOLERANCE = 50;
