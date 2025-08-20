547f3892b1f55ba3ae442c74ebe1a7331083a6cd
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-20 17:44:47 +0300

    Scroll to player score

--- apps/client/src/pages/end-game/action.ts
+++ apps/client/src/pages/end-game/action.ts
@@ -1,10 +1,23 @@
 /* eslint-disable @typescript-eslint/only-throw-error */
+import { jwtDecode } from "jwt-decode";
 import { redirect, type ActionFunctionArgs } from "react-router";
 import api from "../../lib/api";
 
 export default async function action({ request, params }: ActionFunctionArgs) {
+    const json = (await request.json()) as {
+        characters: {
+            id: number;
+            position: {
+                x: number;
+                y: number;
+            } | null;
+        }[];
+        playerToken: string;
+        gameToken: string;
+    };
+
     const response = await api(`games/${params.gameId}/end-game`, {
-        body: JSON.stringify(await request.json()),
+        body: JSON.stringify(json),
         method: "post",
     });
 
@@ -12,5 +25,7 @@ export default async function action({ request, params }: ActionFunctionArgs) {
         throw response;
     }
 
-    return redirect(`/games/${params.gameId}/leaderboard`);
+    const { id }: { id: string } = jwtDecode(json.playerToken);
+
+    return redirect(`/games/${params.gameId}/leaderboard#${id}`);
 }
