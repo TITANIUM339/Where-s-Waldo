4d1afcdc56ee63f5846cf9ffbc700afd6a09a83b
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-21 18:15:07 +0300

    Fix bug

--- apps/server/src/controllers/end-game-controller.ts
+++ apps/server/src/controllers/end-game-controller.ts
@@ -21,14 +21,14 @@ export default {
             verifyJwt(gameToken),
         ]);
 
-        const id = (player as Player).id;
+        const playerId = (player as Player).id;
         const name = (player as Player).name;
         const start = new Date((game as Game).iat * 1000);
 
         const entry = await prisma.leaderboard.upsert({
-            create: { id, name, start, gameId },
+            create: { playerId, name, start, gameId },
             update: { start },
-            where: { id },
+            where: { id: { playerId, gameId } },
         });
 
         res.status(201).json(entry);
