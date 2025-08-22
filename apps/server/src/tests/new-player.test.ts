e8b0972cb35d1f40f9025ef7722fc4ebeb8b2f9d
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-22 15:11:19 +0300

    Test new-player router

---
+++ apps/server/src/tests/new-player.test.ts
@@ -0,0 +1,64 @@
+import express from "express";
+import request from "supertest";
+import { describe, expect, test } from "vitest";
+import errorHandler from "../middleware/error-handler";
+import newPlayerRouter from "../routes/new-player/new-player";
+
+const app = express();
+
+app.use(newPlayerRouter);
+
+app.use(errorHandler);
+
+describe("responds with 400 for bad input", () => {
+    test("GET /new-player", async () => {
+        const response = await request(app).get("/new-player");
+
+        expect(response.status).toBe(400);
+        expect(response.body).toMatchObject({
+            data: [
+                {
+                    location: "query",
+                    msg: "required",
+                    path: "name",
+                    type: "field",
+                    value: "",
+                },
+            ],
+            message: "Bad Request",
+            status: 400,
+        });
+    });
+
+    test("GET /new-player?name=This string is too long", async () => {
+        const response = await request(app).get(
+            "/new-player?name=This string is too long",
+        );
+
+        expect(response.status).toBe(400);
+        expect(response.body).toMatchObject({
+            data: [
+                {
+                    location: "query",
+                    msg: "maximum length is 16",
+                    path: "name",
+                    type: "field",
+                    value: "This string is too long",
+                },
+            ],
+            message: "Bad Request",
+            status: 400,
+        });
+    });
+});
+
+describe("responds with 200 for correct input", () => {
+    test("GET /new-player?name=Player", async () => {
+        const response = await request(app).get("/new-player?name=Player");
+
+        expect(response.status).toBe(200);
+        expect(response.body).toMatchObject({
+            token: /^[\w-]+\.[\w-]+\.[\w-]+$/,
+        });
+    });
+});
