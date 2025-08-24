d9417292e906c1f823e9ba37fe70c796afd4e53e
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-08-24 13:44:46 +0300

    Remove unnecessary code

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -8,7 +8,6 @@ import routes from "./routes/routes.js";
 const app = express();
 
 app.use(helmet());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
